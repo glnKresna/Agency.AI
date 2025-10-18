db9bb722476bd5c8d82931b79cf0000bfab2402e
glnKresna <prabandikakresna@gmail.com>
2025-10-18 13:55:49 +0700

    Init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero';
+import TrustedBy from './components/TrustedBy';
+
+const App = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
+
+  return (
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <Hero />
+      <TrustedBy />
+    </div>
+  )
+}
+
+export default App
